a979879645c768d914d0caf581c267f6d3e33d60
gustavor326 <gustavor326@gmail.com>
2024-05-16 15:11:23 -0300

    adicionando telas

--- javascript/cadastro.js
+++ javascript/cadastro.js
@@ -16,7 +16,7 @@ submitButton.addEventListener('click', function(event) {
 
     
     if (validar()) {
-        window.location.href = 'usuario_profissional.html'; 
+        window.location.href = 'termo_de_acordo.html'; 
     }
 });
 
